0a2d9965eeec13c853f75c8aa2104a8cbb7cac36
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-09-27 17:35:21 +0300

    tr

--- lesson1/home1/src/UserMenu.jsx
+++ lesson1/home1/src/UserMenu.jsx
@@ -3,7 +3,6 @@ import UserContext from './user-context';
 
 class UserMenu extends React.Component {
   render() {
-    console.log(this.context.name);
     return (
       <div className="menu">
         <span className="menu__greeting">{`Hello, ${this.context.name}`}</span>
