bb5373962966e5408649ded37603804fd79056c1
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-09-29 14:32:56 +0300

    try

--- lesson3/home2/src/index.jsx
+++ lesson3/home2/src/index.jsx
@@ -1,13 +1,13 @@
-import { en, es, jp } from './language.actions';
+import { EN, ES, JP, SetLanguage } from './language.actions';
 import { addProduct, removeProduct } from './cart.actions';
 import store from './store';
 
 store.subscribe(() => {
   console.log(store.getState());
 });
-store.dispatch(en());
-store.dispatch(es());
-store.dispatch(jp());
+store.dispatch(SetLanguage(JP));
+store.dispatch(SetLanguage(EN));
+store.dispatch(SetLanguage(ES));
 
 store.dispatch(addProduct({ id: 1, name: 'TV' }));
 store.dispatch(addProduct({ id: 2, name: 'BBQ' }));
--- lesson3/home2/src/language.actions.js
+++ lesson3/home2/src/language.actions.js
@@ -2,12 +2,6 @@ export const EN = 'LANGUAGE/EN';
 export const ES = 'LANGUARE/ES';
 export const JP = 'LANGUAGE/JP';
 
-export const en = () => ({
-  type: EN,
-});
-export const es = () => ({
-  type: ES,
-});
-export const jp = () => ({
-  type: JP,
+export const SetLanguage = language => ({
+  type: language,
 });
