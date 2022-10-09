4d4d36ecf55c1a6e4c07e2ddf3894b060b082a75
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-10-09 12:27:21 +0300

    try4

--- lesson6/task2/src/users/UserInfo.jsx
+++ lesson6/task2/src/users/UserInfo.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { isFethingSelector, userDataSelector } from './users.selectors';
+import Spinner from './Spinner';
 
 const UserInfo = ({ userData, isFetching }) => {
   if (isFetching) {
-    return <span className="spinner"></span>;
+    return <Spinner />;
   }
   if (!userData) {
     return null;
@@ -17,7 +18,7 @@ const UserInfo = ({ userData, isFetching }) => {
       <img alt="User Avatar" src={avatar_url} className="user__avatar" />
       <div className="user__info">
         <span className="user__name">{name}</span>
-        <span className="user__location">{location !== null && `from ${location}`}</span>
+        <span className="user__location">{location}</span>
       </div>
     </div>
   );
